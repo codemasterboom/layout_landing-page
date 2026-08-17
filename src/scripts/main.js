8571ebada97260281379471de3df4079cd30d4be
Vladyslav Hlukhov <vlad.ua@gmail.com>
2026-08-17 15:12:10 +0300

    remove main.js script

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const form = document.getElementById('contactForm');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  // Your submission logic here
-  form.reset();
-});
