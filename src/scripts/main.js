f5956d48ceb94c1417121c2d5f29fd4d341b23bb
Vladyslav Hlukhov <vlad.ua@gmail.com>
2026-08-17 11:57:32 +0300

    fix comments from pull request

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Your submission logic here
+  form.reset();
+});
